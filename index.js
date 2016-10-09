0008b8f9e08833759602f7125e2f850d1f851c2e
Kyle Hardgrave <kyle@kylehardgrave.com>
2016-10-09 12:55:08 -0700

    Mostly there

--- index.js
+++ index.js
@@ -1,168 +1,252 @@
-import _ from './utils'
+/**
+ * @fileoverview A simple dependency injection framework.
+ *
+ * Goals:
+ * - Handle function, constructor, and constant injectors
+ * - Allow asynchronous injectors
+ * - Easy forking for multiple environments
+ * - Allow specifying dependencies by name (robot leg problem)
+ * - Unfilfilled dependencies are failures, not null
+ * - Allow easy debugging of dependency graph
+ *
+ * @flow
+ */
+import q from 'promise-utils'
+import immutable from 'immutable'
 
-const ProviderType = {
-  CTOR: Symbol('constructor'),
-  FACTORY: Symbol('factory'),
-  CONSTANT: Symbol('constant'),
-}
+type ProviderType = 'CONSTANT' | 'CTOR' | 'FN';
 
-const VALID_OPTIONS = ['override', 'using']
+const IMap = immutable.Map
 
-function Options(opts) {
-  this.override = opts.override == null ? false : opts.override
-  this.using = opts.using == null ? null : opts.using
-  for (key in opts) {
-    if (VALID_OPTIONS.indexOf(key) == -1) {
-      throw new Error(`Supplied invalid option: ${key}`)
-    }
-  }
-}
-
+/**
+ * A registry for holding all dependency providers.
+ *
+ * Registries are immutable and should generally be global to an application,
+ * though will also often be forked for different environments, e.g., a unit
+ * testing path is likely to provide different low-level data libraries.
+ *
+ * TKTK This is analagous to the Graph() in Shepherd.
+ */
 class Registry {
-  constructor() {
-    this._providers = new immutable.Map()
+  _providers: IMap<string, Provider>;
+
+  constructor(opt_providers?: IMap<string, Provider>) {
+    this._providers = opt_providers || new IMap<string, Provider>
   }
 
-  ctor(name: string, provider: Class<Object>, deps?: Array<string>, opts?: ProviderOptions) {
-    return this.add(name, ProviderType.CTOR, provider, opt_deps, opt_opts)
+  ctor(name: string, ctor: Function, opt_options?: Object): Registry {
+    assert(typeof ctor == 'function',
+        `Cannot provide nonfunction for constructor provider ${name}`)
+    const deps = getDependencyNames(ctor)
+    return this._add(name, 'CTOR', ctor, deps, opt_options)
   }
 
-  factory(name: string, provider: Function, deps?: Array<string>, opt_opts?: ProviderOptions) {
-    return this.add(name, ProviderType.FACTORY, provider, opt_deps, opt_opts)
+  constant(name: string, constant: any): Registry {
+    assert(constant != null,
+        `Cannot provide null for constant provider ${name}`)
+    return this._add(name, 'CONSTANT', constant, [])
   }
 
-  constant(name: string, provider: any, opt_opts) {
-    return this.add(name, ProviderType.CONSTANT, provider, [], opt_opts)
+  fn(name: string, fn: Function, opt_options?: Object): Registry {
+    assert(typeof fn == 'function',
+        `Cannot provide nonfunction for function provider ${name}`)
+    const dependencies = getDependencyNames(fn)
+    return this._add(name, 'FN', fn, dependencies, opt_options)
   }
 
-  /**
-   * Add a provider.
-   *
-   * @param {string} name
-   * @param {ProviderType} type
-   * @param {(Array.<string>|Object)=} opt_deps
-   * @param {Object=} opt_opts
-   * @return {Registry}
-   */
-  add(name, type, provider, opt_deps, opt_opts) {
-    let deps = Array.isArray(opt_deps) ? opt_deps : provider.$inject
-    let opts = Array.isArray(opt_deps) ? opt_opts : opt_deps
-    deps = deps || []
-    opts = new Options(opts || {})
-
-    const dependencyMap = new Map()
-    deps.forEach((depName) => {
-      const providerName = opts.using && opts.using[depName] || depName
-      dependencyMap.set(depName, providerName)
-    })
-
-    return this._add(name, type, provider, dependencyMap, opts)
-  }
-
-  /**
-   * Add a provider.
-   */
-  _add(name, type, provider, dependencyMap, opts) {
-    if (this._providers.has(name) && !opts.override) {
-      throw new Error(`Attempted to re-register dependency ${name}`)
+  _add(name: string, type: ProviderType, factory: T, deps: Array<string>,
+      options: Object = {}): Registry {
+    let mappedDeps = deps
+    if (options.using) {
+      mappedDeps = deps.map(d => options.using[d] || d)
     }
-
-    const descriptor = new ProviderDescriptor({name, type, provider, dependencyMap}, opts)
-    this._providers.set(name, descriptor)
-    return this
+    const provider = new Provider(name, type, factory, mappedDeps, {
+      isCacheable: !!options.isCacheable
+    })
+    return new Registry(this._providers.set(provider))
   }
 
-  build(name) {
-    return new Injector(this._providers).build(name)
+  buildInjector(): Injector {
+    // TODO validate graph with top-sort
+    return new Injector(this._providers)
   }
 }
 
-function build(name: string, providers: immutable.Map<string, Provider>): Promise<any> {
-  let results = new immutable.Map()
-  if (!this._providers.has(name)) {
-    return Promise.reject(new ProviderNotFound(`Cannot find provider ${name}`))
-  }
-  if (this._results.has(name)) {
-    return Promise.resolve(this._results.get(name))
-  }
+/**
+ * Parse the dependency names out of a function provider.
+ *
+ * First checks for named dependencies in the `$ij` field, used in minified
+ * contexts. Otherwise attempts to parse the dependencies by name from the
+ * function signature, accounting for ES6 classes.
+ */
+function getDependencyNames(fn: Function): Array<string> {
+  if (fn.$ij && Array.isArray(fn.$ij)) return fn.$ij
 
+  const keywordIdx = 0
+  const fnStr = fn.toString()
+  // If this is an ES6 class
+  if (fnStr.startsWith('class')) {
+    const ctorMatch = /\bconstructor\b/.exec(fnStr)
+    // Empty default constructor
+    if (!ctorMatch) return []
+    keywordIdx = ctorMatch.index
+  }
+  const leftParenIdx = fnStr.indexOf('(', keywordIdx)
+  const rightParenIdx = fnStr.indexOf(')', keywordIdx)
+  const paramsStr = fnStr.substring(leftParenIdx + 1, rightParenIdx).trim()
+  if (paramsStr === '') return []
 
+  return params.split(',').map(p => p.trim())
 }
 
+/** The descriptor of a provider. */
+class Provider {
+  /** The global name of the Provider as it exists in the Registry. */
+  name: string;
+
+  /** The type of this provider, used in determining how to build itself. */
+  type: ProviderType;
+
+  /** The actual value of the provider, which can be anything. */
+  factory: T;
+
+  /**
+   * The list of dependencies required by this Provider, identified by their
+   * global names in the Registry.
+   */
+  dependencies: Array<string>;
+
+  /** Whether this Provider's result can be globally cached. */
+  isCacheable: boolean;
+
+  constructor(name: string, type: ProviderType, factory: any,
+      deps: Array<string>, opts={}) {
+    this.name = name
+    this.type = type
+    this.factory = factory
+    this.dependencies = deps
+    this.isCacheable = !!opts.isCacheable
+    Object.seal(this)
+  }
+}
+
+/**
+ * An injector for building dependencies from a registry.
+ *
+ * Injectors have state and will typically live for the lifecycle of an
+ * application. It handles caching singleton dependencies, e.g., a database
+ * connection, but will build non-cacheable dependencies once per build call.
+ *
+ * Injectors must be valid at the time of instantiation, and so should only be
+ * built with Registry#buildInjector().
+ *
+ * TKTK This is analagous to a Builder() in Shepherd.
+ */
 class Injector {
-  constructor(providers: immutable.Map<string, Provider>) {
+  _providers: IMap<string, Provider>;
+  _cache: IMap<string, any>;
+
+  constructor(providers: IMap<string, Provider>) {
     this._providers = providers
-    this._results = new immutable.Map()
+    this._cache = new IMap()
   }
 
+  /**
+   * Build a dependency from the registry.
+   *
+   * This method will build a dependency (and its subdependencies) anew each
+   * time it's called unless the dependency has specifically indicated that it
+   * is cacheable. However it will not build a dependency more than once per
+   * call, even if the same dependency is required multiple times.
+   */
   build(name: string): Promise<any> {
     if (!this._providers.has(name)) {
-      return Promise.reject(new ProviderNotFound(
-          `Cannot find provider for "${name}"`))
-    }
-
-    if (this._results.has(name)) {
-      return Promise.resolve(this._results.get(name))
+      return Promise.reject(new Error(`Provider not found for ${name}`))
     }
 
     const provider = this._providers.get(name)
-    const resultsMap = new Map()
-    const promises = _.map(provider.getDependencyMap(), (providerName, dependencyName) => {
-      const promise = isLiteral(providerName) ?
-          Promise.resolve(providerName) :
-          this.build(providerName)
-
-      return promise.then((result) => {
-        resultsMap.set(dependencyName, result)
-      })
-    })
-
-    return Promise.all(promises).then(() => {
-      const result = provider.buildWithDependencies(resultsMap)
-      this._results.set(name, result)
-      return result
-    })
-  }
-}
-
-class ProviderDescriptor {
-  constructor({name, provider, type, dependencyMap}, {shouldCache}) {
-    this._name = name
-    this._provider = provider
-    this._providerType = providerType
-    this._dependencyMap = dependencyMap
+    return this._build(provider, name)
+        .then((results) => results.get(provider.name))
+        .catch((err) => {
+          if (err instanceof ProviderNotFound) {
+            err.parents.push(name)
+            const chain = err.parents.join(' → ')
+            throw new Error(err.message + `(in dependency chain: ${chain})`)
+          }
+          throw err
+        })
   }
 
-  buildWithDependencies(resultsMap) {
-    return new Promise((resolve, reject) => {
-      switch (this._providerType) {
-        case ProviderType.CTOR:
-          return resolve(new this._provider(resultsMap))
-        case ProviderType.FACTORY:
-          return resolve(this._provider(resultsMap))
-        case ProviderType.CONSTANT:
-          return resolve(this._provider)
-        default:
-          reject(new Error(`Invalid provider type ${this._providerType}`))
+  _build(provider: Provider): Promise<IMap<string, any>> {
+    if (this._cache.has(provider.name)) {
+      return this._cache.get(provider.name)
+    }
+
+    const childPromises = provider.dependencies.map((depName) => {
+      const depProvider = this._providers.get(depName)
+      if (!depProvider) {
+        // TODO clean up API for parents
+        const err = new ProviderNotFound(`Provider not found for ${depName}`)
+        err.parents = [provider.name]
+        return Promise.reject(err)
       }
+
+      return this._build(depProvider)
+        .catch((err) => {
+          if (err instanceof ProviderNotFound) {
+            err.parents.push(provider.name)
+          }
+          throw err
+        })
     })
+
+    return Promise.all(childPromises)
+      .then((depResults) => {
+        const results = new IMap().merge(...depResults)
+        return this._buildProvider(provider, results)
+          .then((result) => {
+            if (provider.isCacheable) {
+              this._cache = this._cache.set(provider.name, result)
+            }
+            return results.set(provider.name, result)
+          })
+      })
+  }
+
+  _buildProvider(provider: Provider, results: IMap<string, any>): Promise<any> {
+    const args = provider.dependencies.map(d => results.get(d))
+    switch (provider.type) {
+      case 'CONSTANT':
+        return Promsie.resolve(provider.factory)
+      case 'CTOR':
+        return Promise.resolve(new provider.factory(...args))
+      case 'FN':
+        return Promise.resolve(provider.factory(...args))
+    }
   }
 }
 
-function literal(value) {
-  return {__literal: value}
+class ProviderNotFound extends Error {
+  _parents: ?Array<string>;
+
+  addParent(name: string): ProviderNotFound {
+    if (!this._parents) this._parents = [name]
+    else this._parents.push(name)
+    return this._parents
+  }
+
+  get parents(): Array<string> {
+    return this._parents || []
+  }
 }
 
-function isLiteral(test) {
-  return !!(test && test.__literal)
-}
-
-function getLiteral(test) {
-  return test && test.__literal || null
+function assert<T|?T>(val: T|?T, msg: string): T {
+  if (!val) {
+    throw new Error(msg)
+  }
+  return val
 }
 
 export default {
-  literal,
   Registry,
-  ProviderType,
 }
---
+++ test.js
@@ -0,0 +1,27 @@
+import test from 'ava'
+
+import ij from './index'
+
+test('Registry#ctor() fails if not a function', t => {
+  const r = new ij.Registry()
+  const A = {}
+  t.throws(() => { r.ctor('a', A) })
+})
+
+test('Registry#fn() fails if not a function', t => {
+  const r = new ij.Registry()
+  const a = {}
+  t.throws(() => { r.fn('a', a) })
+})
+
+test('Registry#constant() fails if null', t => {
+  const r = new ij.Registry()
+  const a = null
+  t.throws(() => { r.constant('a', a) })
+})
+
+test('Registry#constant() fails if null', t => {
+  const r = new ij.Registry()
+  const a = undefined
+  t.throws(() => { r.constant('a', a) })
+})
